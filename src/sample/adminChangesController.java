7aceeb995ccabdf3c0244093158834c79a40229b
Helga <helgagunndis@gmail.com>
2021-04-13 22:32:45 +0000

    addTour sem admin eitthvað komið

--- src/sample/adminChangesController.java
+++ src/sample/adminChangesController.java
@@ -1,9 +1,11 @@
 package sample;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,11 +15,16 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class adminChangesController {
+public class adminChangesController implements Initializable {
 
     private TourDataFactory tourdataFactory = new TourDataFactory();
-    private ObservableList<Tour> tours = tourdataFactory.getTours();
+   //private ObservableList<Tour> tours = tourdataFactory.getTours();
+   private ObservableList<Tour> tours = FXCollections.observableArrayList();
 
     @FXML
     private Button buttonDeleteTour;
@@ -35,6 +42,15 @@ public class adminChangesController {
     private TextField textTourInfo;
     @FXML
     private TextField textTourID;
+    @FXML
+    private TextField textTourServices;
+    @FXML
+    private TextField textTourRegion;
+    @FXML
+    private TextField textTourDuration;
+   // @FXML
+    //private ListView tourListView;
+
 
 
     public void backToMainPageButtonAdminCOnAction(ActionEvent actionEvent) throws IOException {
@@ -46,13 +62,13 @@ public class adminChangesController {
     }
 
     public void buttonAddTourOnAction(ActionEvent event) {
-        String tourName = textTourName.getText();
-        String tourDate = textTourDate.getText();
         String tourPrice = textTourPrice.getText();
         String maxBooking = textMaxBooking.getText();
-        String tourInfo = textTourInfo.getText();
-
-        System.out.println(tourName);
+        String tourDuration = textTourDuration.getText();
+        int price = Integer.parseInt(tourPrice);
+        int spots = Integer.parseInt(maxBooking);
+        int duration = Integer.parseInt(tourDuration);
+        tours.add(new Tour(textTourName.getText(), textTourInfo.getText(), LocalDate.of(2021,5,6),spots,price,textTourRegion.getText(),duration,textTourServices.getText()));
     }
 
     public void buttonDeleteTourOnAction(ActionEvent event) {
@@ -63,6 +79,12 @@ public class adminChangesController {
                 tours.remove(tour);
             }
         });
-        //System.out.print(enteredTourID);
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tours = tourdataFactory.getTours();
+        //tourListView.setItems(tours);
     }
 }
